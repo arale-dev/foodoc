e40e7ba5f71528b9646bc0afed7d76bc62def5d1
arale-dev <9801gksquf@gmail.com>
2021-11-01 23:33:14 +0900

    [feat] add footer and favicon

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,42 @@
 <script>
+	import Footer from './components/Footer.svelte'
 	export let name;
 </script>
+
+<svelte:head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</svelte:head>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
+<Footer />
 
 <style>
+	:global(html) {
+		font-size: 62.5%;
+	}
+
+	@media (max-width: 768px) {
+		:global(html) {
+			font-size: 50%;
+		}
+	}
+	@media screen and (min-width: 768px) and (max-width: 1023px) {
+    	:global(html) {
+			font-size: 100%;
+		}
+    }
+    @media screen and (min-width: 1024px) {
+    	:global(html) {
+			font-size: 200%;
+		}
+    }
+
 	main {
 		text-align: center;
-		padding: 1em;
+		padding: 2rem;
 		max-width: 240px;
 		margin: 0 auto;
 	}
@@ -22,9 +48,4 @@
 		font-weight: 100;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ src/components/Footer.svelte
@@ -0,0 +1,19 @@
+<footer>
+    <div class="copyright">
+        ©2021 AIST ㅣ All rights reserved
+    </div>
+</footer>
+
+<style>
+    footer{
+        padding: 2rem;
+        text-align: center;
+    }
+    .copyright{
+        color: #aaa;
+        font-size: 0.6rem;
+        display: inline-block;
+        padding: 1rem 2rem;
+        border-top: 1px solid #ddd;
+    }
+</style>
